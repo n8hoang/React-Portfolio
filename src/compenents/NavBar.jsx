0b54c1763b9bbf289531b8717d0b7fa91342a9dc
Nathan Hoang <sliqq123@gmail.com>
2023-11-08 17:36:20 -0800

    built router, navbar , and init file structure

---
+++ src/compenents/NavBar.jsx
@@ -0,0 +1,51 @@
+import {FaBars, FaTimes} from 'react-icons/fa'
+import { Link, useLocation } from 'react-router-dom';
+
+const NavBar = () => {
+    const currentPage = useLocation().pathname;
+    const links = [
+        {
+            id: 1,
+            link: '/',
+            name: 'Home'
+        },
+        {
+            id: 2,
+            link: '/portfolio',
+            name: 'Portfolio'
+        },
+        {
+            id: 3,
+            link: '/contact',
+            name: 'Contact'
+        },
+        {
+            id: 4,
+            link: '/resume',
+            name: 'Resume'
+        },
+    ]
+
+
+    return <div className='flex justify-between items-center w-full h-20 px-4 text-white fixed bg-black'>
+        <div>
+            <h1 className=' text-5xl font-signature ml-2'>Nathan</h1>
+        </div>
+
+        <ul className='flex'>
+            {/* map thorugh links object to create a <Link for each route.*/}
+            {links.map(({ id, link, name}) => (
+                <Link 
+                key={id}
+                to={link}
+                // Change color to white if navtab is active
+                className = {currentPage === link ? 'px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-200;' : ' px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'} 
+                >
+                    {name}
+                </Link>
+            ))}
+        </ul>
+    </div>
+}
+
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Define the routes for the application
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/about', element: <About /> },
+      { path: '/portfolio', element: <Portfolio /> },
+      { path: '/contact', element: <Contact /> },
+      { path: '/resume', element: <Resume /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the <RouterProvider> with your router into the root
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
