a74267669f778616510e36f5340f6508f969f037
Nathan Hoang <sliqq123@gmail.com>
2023-12-24 13:15:29 -0800

    made contact form functional with backend nodemailer

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(express.json()); // For parsing application/json
+app.use(cors()); // Cross Origin Resource Sharing for nodemailer
+
+const port = process.env.PORT || 3001;
+
+//nodemailer setup
+const transporter = require('./mailer/mailer');
+
+// Contact Me Email
+app.post('/send-message', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: process.env.EMAIL, // sender address
+    to: process.env.SEND_TO, // list of receivers
+    subject: 'Portfolio - Contact Form', // Subject line
+    text: `From: ${name}\nEmail: ${email}\nMessage: ${message} `, // plain text body
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
